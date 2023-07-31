800872dadb35713316edba9a6cbcea7c0bcec7ec
Leoyby <hanlan@njfu.edu.cn>
2023-07-31 12:50:30 +0800

    update

--- README.md
+++ README.md
@@ -10,22 +10,36 @@
 2. 依赖：
 
    - requests
+   - ping3
 
    ```bash
-   pip install requests
+   pip install requests ping3
    ```
 
 3. Ping和连接wifi的实现是调用了系统工具。Ping通过调用系统的`ping`实现；wifi连接目前只实现了Windows，通过调用`netsh`
 
 4. 使用方法
-    ```
+   ```
     自动检测网络状况，并且连接校园网
 
     options:
-    -h, --help           show this help message and exit
-    --host HOST          ping的主机
-    --wifi WIFI          wifi名称
-    --url URL            登录地址
-    --username USERNAME  用户名 | NJFU-WiFi: w<学号> | CMCC-EDU: w<学号>@cmcc
-    --password PASSWORD  密码 | 通常是身份证后六位
-    ```
+      -h, --help           show this help message and exit
+      --host HOST          ping的主机，用于检测网络是否连通，default=www.baidu.com
+      --wifi WIFI          wifi名称
+      --url URL            登录地址
+      --username USERNAME  用户名 | NJFU-WiFi: w<学号> | CMCC-EDU: w<学号>@cmcc
+      --password PASSWORD  密码 | 通常是身份证后六位
+      --failure FAILURE    连续失败的次数，default=4，>=1
+      --native-ping        使用本地的ping，否则使用ping3模块
+   ```
+5. 例子：cmd脚本，连接NJUF-WiFi
+   ```cmd
+   "D:\Program Files\Python311\python.exe" ^
+   D:\codefile\code_py\njfu_wifi\main.py ^
+   --wifi NJFU-WiFi ^
+   --url http://121.248.150.37 ^
+   --username w<学号> ^
+   --password <身份证后六位> ^
+   --native-ping
+   pause
+   ```
--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 from time import sleep
-from njfu_wifi import login, ping, connect2wifi
+import njfu_wifi
 import logging
 import argparse
+import sys
 
 def parse_arg():
     despription = "自动检测网络状况，并且连接校园网"
     parse = argparse.ArgumentParser(description=despription)
-    parse.add_argument("--host", help="ping的主机，用于检测网络是否连通", default="www.baidu.com")
+    parse.add_argument("--host", help="ping的主机，用于检测网络是否连通，default=www.baidu.com", default="www.baidu.com")
     parse.add_argument("--wifi", help="wifi名称", required=True)
     parse.add_argument("--url", help="登录地址", required=True)
     parse.add_argument("--username", help="用户名 | NJFU-WiFi: w<学号> | CMCC-EDU: w<学号>@cmcc", required=True)
     parse.add_argument("--password", help="密码 | 通常是身份证后六位", required=True)
-    return parse.parse_args()
+    parse.add_argument("--failure", type=int ,help="连续失败的次数，default=4，>=1", default=4)
+    parse.add_argument("--native-ping", action="store_true", help="使用本地的ping，否则使用ping3模块")
+    args = parse.parse_args()
+    if args.failure < 1:
+        args.failure = 4
+        print("Warning: failure的值应当不小于1，已采用默认值4", file=sys.stderr)
+    
+    return args
 
 
 if __name__ == "__main__":
     
     args = parse_arg()
     
-    failure = 3
+    if args.native_ping:
+        ping = njfu_wifi.ping
+    else:
+        import ping3
+        ping = ping3.ping
+        
+    failure = args.failure - 1
     succ = 0
     logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)
     while True:
         try:
             result = ping(args.host)
         except:
-            result = None
+            result = False
         if result:
             failure = 0
             succ = min(10, succ + 1)
@@ -34,12 +48,12 @@ if __name__ == "__main__":
             succ = 0
             failure = failure + 1
             logging.warning("Fail Connenct to Internet")
-            if failure >= 4:
+            if failure >= args.failure:
                 logging.info("Login Cmcc")
                 try:
-                    connect2wifi(args.wifi)
+                    njfu_wifi.connect2wifi(args.wifi)
                     sleep(8)
-                    login(args.username, args.password, args.url)
+                    njfu_wifi.login(args.username, args.password, args.url)
                     logging.info("Login Sucess!")
                 except:
                     logging.error("Login Fail!")
